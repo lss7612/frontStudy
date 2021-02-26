8e1fb4e8cd073a786cd439ed2762dae07251d807
lss7612 <lss76121@naver.com>
2021-02-26 22:21:33 +0900

    0226

--- browser/chapter02/prac/007.js
+++ browser/chapter02/prac/007.js
@@ -2,7 +2,16 @@ const target = document.querySelector('.target');
 const width = target.offsetWidth;
 const heigth = target.offsetHeight;
 
-window.addEventListener('mousemove', (e) => {
+const coordinate = document.querySelector('.coordinate');
+const lineX = document.querySelector('.horizental');
+const lineY = document.querySelector('.vertical');
+
+document.addEventListener('mousemove', (e) => {
   target.style.left = `${e.clientX - width / 2}px`;
   target.style.top = `${e.clientY - heigth / 2}px`;
+  coordinate.style.left = `${e.clientX + 20}px`;
+  coordinate.style.top = `${e.clientY + 20}px`;
+  coordinate.innerHTML = `${e.clientX}px, ${e.clientY}px`;
+  lineX.style.top = `${e.clientY}px`;
+  lineY.style.left = `${e.clientX}px`;
 });
